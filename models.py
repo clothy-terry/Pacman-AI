f13c6562fc41f8d6cfd9a64cda0724d9c168e521
TerryLiu <terryliu@berkeley.edu>
2023-04-26 21:30:03 -0700

    Finished Q2

--- models.py
+++ models.py
@@ -67,6 +67,12 @@ class RegressionModel(object):
     """
     def __init__(self):
         # Initialize your model parameters here
+        self.w1 = nn.Parameter(1, 512)# first argument should be dim (x)
+        self.b1 = nn.Parameter(1, 512)
+        self.w2 = nn.Parameter(512, 1)
+        self.b2 = nn.Parameter(1, 1)
+        self.learnRate = 0.01
+
         "*** YOUR CODE HERE ***"
 
     def run(self, x):
@@ -79,6 +85,10 @@ class RegressionModel(object):
             A node with shape (batch_size x 1) containing predicted y-values
         """
         "*** YOUR CODE HERE ***"
+        
+        h1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        predicted_y = nn.AddBias(nn.Linear(h1, self.w2), self.b2)
+        return predicted_y
 
     def get_loss(self, x, y):
         """
@@ -91,12 +101,27 @@ class RegressionModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        predicted_y = self.run(x)
+        loss = nn.SquareLoss(predicted_y, y)
+        return loss
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        batch_size = 200
+        loss = 1
+        while loss >= 0.02:
+            for x, y in dataset.iterate_once(batch_size):
+                loss = self.get_loss(x,y)
+                gw1, gw2, gb1, gb2 = nn.gradients(loss, [self.w1, self.w2, self.b1, self.b2])
+                self.w1.update(gw1, -self.learnRate)
+                self.w2.update(gw2, -self.learnRate)
+                self.b1.update(gb1, -self.learnRate)
+                self.b2.update(gb2, -self.learnRate)
+                loss = nn.as_scalar(loss)
+
 
 class DigitClassificationModel(object):
     """
